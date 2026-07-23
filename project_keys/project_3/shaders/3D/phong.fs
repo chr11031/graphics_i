0deaae67e3095fbbf3373e808c25b8b421164781
dwc <chr11031@byui.edu>
2026-07-23 00:14:54 -0400

    Next stage of P3 setup

--- project_keys/project_3/shaders/3D/phong.fs
+++ project_keys/project_3/shaders/3D/phong.fs
@@ -3,29 +3,44 @@ precision highp float;
 
 const vec3 light_pos = vec3(-5.0, 5.0, 5.0);
 
-smooth in vec3 interp_normal;
 smooth in vec3 interp_pos;
+smooth in vec3 interp_normal;
+smooth in vec2 interp_uv;
 
 layout (location=0) out vec4 out_color;
 
 void main() {
 
+	int uv_checker_x = int(interp_uv.x * 8.0);
+	int uv_checker_y = int(interp_uv.y * 8.0);
+	
+	vec3 base_color = vec3(1.0, 1.0, 1.0);
+	if (uv_checker_x % 2 == uv_checker_y % 2)
+	{
+		base_color = vec3(1.0, 0.0, 0.0);
+	}
+	
+	
+
 	// Ambient
 	vec3 color = vec3(0.1, 0.1, 0.1);
 
+
+	
 	// Diffuse
 	vec3 light_dir = normalize( light_pos - interp_pos.xyz );	
 	vec3 normal = normalize(interp_normal);
+
 	float align = dot(light_dir, normal);
 	align = max(0.0, align);
-	color += align * vec3(1.0, 0.5, 0.5);
+	color += align * base_color;
 	
 	// Specular
 	vec3 reflected = reflect(-light_dir, interp_normal); 
 	vec3 look_dir = normalize(interp_pos);
 	align = dot(look_dir, -normalize(reflected));
 	align = max(0.0, align);
-	align = pow(align, 128.0);
+	align = pow(align, 256.0);
 	color += align * vec3(1.0, 1.0, 1.0);
 
 	
--- project_keys/project_3/shaders/3D/phong.vs
+++ project_keys/project_3/shaders/3D/phong.vs
@@ -2,6 +2,7 @@
 precision highp float;
 layout(location=0) in vec3 pos;
 layout(location=1) in vec3 normal;
+layout(location=2) in vec2 uv;
 
 
 layout(std140) uniform phong_data
@@ -11,15 +12,18 @@ layout(std140) uniform phong_data
 	mat4 model;
 };
 
-smooth out vec3 interp_normal;
 smooth out vec3 interp_pos;
-
+smooth out vec3 interp_normal;
+smooth out vec2 interp_uv;
 
 void main()
 {
 	interp_normal = (view * model * vec4(normal, 0.0)).xyz;
+
 	vec4 tmp = view * model * vec4(pos, 1.0);
 	interp_pos = tmp.xyz;
+
+	interp_uv = uv;
 	
 	gl_Position = proj * tmp;								
 }
